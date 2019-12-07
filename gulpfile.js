09a389b1103ef2819c541f9ba968e57ec36f0c62
plupi <you@example.com>
2019-12-07 03:27:11 +0200

    started

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+
+
+
+
+
+gulp.task('hello', function(done) {
+  console.log('Hello world');
+  done();
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+ 
+gulp.task('minify-css', () => {
+  return gulp.src('./*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('css'));
+    
+});
+
+
+ 
